5866103413c69298dfbfa60a52edbafba2704896
meanzooo <80101715+meanzooo@users.noreply.github.com>
2021-05-26 19:35:04 +0900

    Add files via upload

---
+++ forproject/SavePost.php
@@ -0,0 +1,23 @@
+<?php 
+    $con = mysqli_connect("localhost", "meanzoo", "buyby2020!", "meanzoo");
+    mysqli_query($con,'SET NAMES utf8');
+
+    $TITLE = $_POST["bookName"];
+    $SUBTITLE = $_POST["authorName"];
+    $PRICE = $_POST["detailMemo"];
+    $CONTENT = $_POST["priceSetting"];
+    $WRITER = 2;
+    //WRITER 수정 필요
+
+    $sql = "INSERT INTO POST(TITLE,SUBTITLE,PRICE,CONTENT,WRITER) ";
+    $sql .= "VALUES('$TITLE','$SUBTITLE','$PRICE','$CONTENT','$WRITER')";
+
+    $statement = $con -> prepare($sql);
+    mysqli_stmt_execute($statement);
+
+    $response = array();
+    $response["success"] = true;
+ 
+   
+    echo json_encode($response);
+?>
