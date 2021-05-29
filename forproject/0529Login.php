0163640e699e7d501ed350583066071d5e9254c5
meanzooo <80101715+meanzooo@users.noreply.github.com>
2021-05-29 23:33:01 +0900

    Update 0529Login.php

--- forproject/0529Login.php
+++ forproject/0529Login.php
@@ -1,23 +1,15 @@
 <?php
     //세션 실행문
     session_start();
-    $_SESSION['nickName'] = $nickName;
 
     $con = mysqli_connect("localhost", "meanzoo", "buyby2020!", "meanzoo");
     mysqli_query($con,'SET NAMES utf8');
 
     $userID = $_POST["userID"]; 
     $userPassword = $_POST["userPassword"];
-    // $userID = "123";
-    // $userPassword = "123";
+    $_SESSION['userID'] = $_POST['userID'];
+    $_SESSION['userPassword'] = $_POST['userPassword'];
     
-    // //작성자 불러오는 쿼리문
-    // $loadwriter = mysqli_prepare($con, "SELECT userName FROM STD
-    //                                 WHERE userID = '$userID' AND '$userPassword'");
-    // mysqli_stmt_execute($loadwriter);
-    // mysqli_stmt_fetch($loadwriter);
-    // $_SESSION['nickName'] = $loadwriter;
-
     //로그인 쿼리문
     $statement = mysqli_prepare($con, "SELECT * FROM STD WHERE userID = ? AND userPassword = ?");
     mysqli_stmt_bind_param($statement, "ss", $userID, $userPassword);
@@ -38,6 +30,5 @@
         $response["userName"] = $userName;       
     }
 
-    // echo print_r($_SESSION['nickName']);
     echo json_encode($response);
 ?>
