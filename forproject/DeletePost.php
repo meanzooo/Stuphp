e8b8297086afd4c719c12f8a0288fe89b510f2e8
meanzooo <80101715+meanzooo@users.noreply.github.com>
2021-05-30 23:39:41 +0900

    Add files via upload

---
+++ forproject/DeletePost.php
@@ -0,0 +1,16 @@
+<?php
+	$con = mysqli_connect("localhost", "meanzoo", "buyby2020!", "meanzoo");
+    mysqli_query($con,'SET NAMES utf8');
+
+    $bno = $_GET['idx']; //받아온 idx값을 선택해 게시글 삭제
+    
+    $sql("DELETE FROM POST WHERE idx='$bno'");
+    
+    $statement = $con -> prepare($sql);
+    mysqli_stmt_execute($statement);
+
+    $response = array();
+    $response["success"] = true;
+ 
+    echo print_r($statement);
+?>
