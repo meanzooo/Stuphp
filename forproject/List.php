d579c54d6450af6f79f8a258107e1f47eb0a2284
seon0522 <61931341+seon0522@users.noreply.github.com>
2021-06-21 17:44:42 +0900

    buybySchool

--- forproject/List.php
+++ forproject/List.php
@@ -1,11 +1,18 @@
 <?php
 	$con = mysqli_connect("localhost", "meanzoo", "buyby2020!", "meanzoo");
-	$result = mysqli_query($con, "SELECT * FROM POST;");
+	$result = mysqli_query($con, "SELECT * FROM POST order by DATE DESC");
 
 	$response = array();
 
-	while($row = mysqli_fetch_array($result)) {
-			array_push($response, array("POSTNUM"=>$row[0],"TITLE"=>$row[1], "PRICE"=>$row[3], "WRITER"=>$row[5], "DATE"=>$row[6]));
+
+//    POSTNUM - 게시글 고유번호
+//    TITLE - 글 제목
+//    SUBTITLE - 책의 저자
+//    PRICE - 가격
+//    CONTENT - 소개글
+
+		while($row = mysqli_fetch_array($result)) {
+			array_push($response, array("POSTNUM"=>$row[0],"TITLE"=>$row[1], "SUBTITLE"=>$row[2],"PRICE"=>$row[3], "CONTENT"=>$row[4], "DATE"=>$row[6]));
 			// == response["TITLE"] = $row[1]
 		}
 
